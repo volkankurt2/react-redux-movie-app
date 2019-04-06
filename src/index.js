752e8ca2000b5ae92e089ce96f687913e12f0d63
Volkan Kurt <volkankurt2@gmail.com>
2019-04-06 22:27:08 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -4,6 +4,18 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import { createStore, applyMiddleware } from "redux";
+import thunk from 'redux-thunk';
+import rootReducer from './reducers/rootReducer';
+import { composeWithDevTools } from "redux-devtools-extension";
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(
+    applyMiddleware(thunk)
+  )
+);
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
