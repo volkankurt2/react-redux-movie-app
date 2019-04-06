b9b9f9e0ea5c3b1cd80725ab5372c42e466e66b4
Volkan Kurt <volkankurt2@gmail.com>
2019-04-06 22:34:36 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import thunk from 'redux-thunk';
 
 import { createStore, applyMiddleware } from "redux";
-import thunk from 'redux-thunk';
 import rootReducer from './reducers/rootReducer';
 import { composeWithDevTools } from "redux-devtools-extension";
 
